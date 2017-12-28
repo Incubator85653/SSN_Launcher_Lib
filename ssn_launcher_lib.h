b4ea4dd82e4535796f8e9385b343b01354da3898
Incubator85653 <2bemperor47@gmail.com>
2017-12-29 00:15:05 +0800

    Move code implementation

--- ssn_launcher_lib.h
+++ ssn_launcher_lib.h
@@ -1,20 +1,4 @@
 #ifndef SSN_LAUNCHER_H
 #define SSN_LAUNCHER_H
 
-#include <string>
-#include <iostream>
-#include <QCoreApplication>
-
-using namespace std;
-
-namespace SSN_Launcher_Lib_Static{
-    namespace Environment{
-        QString getWorkingDirectory();
-    }
-    namespace Type{
-        QString qString_fromWstring(wstring input);
-        wstring wstring_fromQString(QString input);
-    }
-}
-
 #endif // SSN_LAUNCHER_H
